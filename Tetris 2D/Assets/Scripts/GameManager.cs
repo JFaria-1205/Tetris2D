d5f882e5c4af7191c6c3f46cb6823a3af660d37d
1205-Company-Repos <151981570+1205-Company-Repos@users.noreply.github.com>
2024-05-14 16:32:48 -0400

    Added UI
    Added level and score UI, as well as functionality to update UI as needed

--- Tetris 2D/Assets/Scripts/GameManager.cs
+++ Tetris 2D/Assets/Scripts/GameManager.cs
@@ -2,25 +2,40 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] LayerMask bounds;
+    [SerializeField] Text UI_Score;
+    [SerializeField] Text UI_Level;
     public int currentLevel { get; private set; }
     public float currentGravity { get; private set; }
     private List<float> gravityValues = new List<float>();
     private int totalLinesCleared = 0;
     private int linesClearedInLevel = 0;
+    private int playerScore = 0;
     BlockSpawner blockSpawner;
     GameObject currentBlock;
 
 
     void Start()
     {
+        InitializeGame();        
+    }
+
+    private void InitializeGame()
+    {
+        playerScore = 0;
+        UpdateScoreUI();
+
         PopulateGravityList();
         UpdateLevelAndGravity(1);
+        UpdateLevelUI();
+
         blockSpawner = GetComponent<BlockSpawner>();
         SpawnBlock();
     }
@@ -37,15 +52,46 @@ public class GameManager : MonoBehaviour
         RowsCleared(out int amountOfRowsCleared);
         Debug.Log("Rows Cleared: " + amountOfRowsCleared);
 
-        //update level and speed
-        if (linesClearedInLevel >= 10 && currentLevel < 15)
+        if (amountOfRowsCleared > 0)
         {
-            //UpdateLevelAndSpeed();
-        }
+            AwardScorePoints(amountOfRowsCleared);
+            linesClearedInLevel += amountOfRowsCleared;
+            totalLinesCleared += amountOfRowsCleared;
 
+            if (linesClearedInLevel >= 10 && currentLevel < 15)
+                UpdateLevelAndGravity();
+        }
 
         //spawn next block
         Invoke("SpawnBlock", 0.75f);
+    }
+
+    private void AwardScorePoints(int numberOfRowsCleared)
+    {
+        int pointMultiplier;
+
+        switch (numberOfRowsCleared)
+        {
+            default:
+                pointMultiplier = 100;
+                break;
+            case 1: //single
+                pointMultiplier = 100;
+                break;
+            case 2: //double
+                pointMultiplier = 300;
+                break;
+            case 3: //triple
+                pointMultiplier = 500;
+                break;
+            case 4: //tetris
+                pointMultiplier = 800;
+                break;
+        }
+
+        playerScore += pointMultiplier * currentLevel;
+
+        UpdateScoreUI();
     }
 
     private void RowsCleared(out int amountCleared)
@@ -106,7 +152,7 @@ public class GameManager : MonoBehaviour
             {
                 foreach (Transform child in blocksToBeCleared)
                 {
-                    Debug.Log("Child to destroy: Child Name: " + child.name + " | Parent Name: " + child.parent.gameObject.name + " | Position: " + child.position);
+                    //Debug.Log("Child to destroy: Child Name: " + child.name + " | Parent Name: " + child.parent.gameObject.name + " | Position: " + child.position);
                     Destroy(child.gameObject);
                 }
                 amountCleared++;
@@ -124,7 +170,7 @@ public class GameManager : MonoBehaviour
             currentLevel = level;            
         }                      
         else
-            currentLevel += 1;
+            currentLevel = Mathf.Clamp(currentLevel+1, 1, 15);
 
         currentGravity = gravityValues[currentLevel-0];
 
@@ -149,4 +195,16 @@ public class GameManager : MonoBehaviour
         gravityValues.Add(1.46f);     // 14
         gravityValues.Add(2.36f);     // 15
     }
+
+    #region UI_Functions
+    private void UpdateScoreUI()
+    {
+        UI_Score.text = ("Score: " + playerScore.ToString());
+    }
+
+    private void UpdateLevelUI()
+    {
+        UI_Level.text = ("Level: " + currentLevel.ToString());
+    }
+    #endregion
 }
