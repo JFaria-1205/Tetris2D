ce645dec7584fe4685f5100328b3f0cfca1afdeb
Jason Faria <jasonfaria01@gmail.com>
2024-06-03 03:23:22 -0400

    Update Notes.txt + Build
    Added notable bug to notes, where block removal has an issue if there is a gap between rows cleared
    
    Built the game and reorganized project to accommodate build.

---
+++ Project Files/Tetris 2D/Assets/Scripts/MM_Manager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MM_Manager : MonoBehaviour
+{
+    [SerializeField] Text HighScore_UI;
+    [SerializeField] GameObject HighScoreDataSaverObject;
+    [SerializeField] GameObject controls_Menu;
+
+    private void Start()
+    {
+        SetHighScoreUI();
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quitting game...");
+    }
+
+    public void ResetButton()
+    {
+        FindObjectOfType<HighScoreData>().SaveNewHighScore(0);
+        SetHighScoreUI();
+    }
+
+    public void ControlsMenuButton()
+    {
+        controls_Menu.SetActive(true);
+    }
+
+    public void ControlsMenuBackButton()
+    {
+        controls_Menu.SetActive(false);
+    }
+
+    public void SetHighScoreUI()
+    {
+        if(FindObjectOfType<HighScoreData>() == null)
+        {
+            Instantiate(HighScoreDataSaverObject);
+        }
+        HighScore_UI.text = "HighScore: " + FindObjectOfType<HighScoreData>().GetHighScore().ToString();
+    }
+}
